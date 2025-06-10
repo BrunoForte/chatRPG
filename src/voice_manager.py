e58c4d3475d549ed669409829e25d976342cf29e
BrunoForte <bruno@fforte.biz>
2025-06-09 21:09:25 -0300

    voice_manager created and requirements updated

--- src/voice_manager.py
+++ src/voice_manager.py
@@ -6,11 +6,15 @@ class TTSManager:
     audio_manager = AudioManager()
     azuretts_manager = AzureTTSManager()
 
-    user_voice_name = None
+    user_voice_name = "pt-BR-AntonioNeural"
 
     def __init__(self):
         file_path = self.azuretts_manager.text_to_audio("Activated!") # Say something when the app starts
         self.audio_manager.play_audio(file_path, True, True, True)
 
-    def update_voice_name(voice_name):
-        user_voice_name = voice_name
+    def update_voice_name(self, voice_name):
+        self.user_voice_name = voice_name
+
+    def text_to_audio(self, text):
+        tts_file = self.azuretts_manager.text_to_audio(text, self.voice_name)
+        self.audio_manager.play_audio(tts_file, True, True, True)
